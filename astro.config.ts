b86b31c2d8cc31443a6727e4487fa4771ad41b5f
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-11 23:03:10 -0300

    chore(astro.config): 🎨 update astro.config from mjs to ts

---
+++ astro.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // Remember to change this URL
+  site: 'https://example.com',
+  integrations: [],
+});
